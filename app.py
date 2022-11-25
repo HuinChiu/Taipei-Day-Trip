5d793093cbb9f573565c2d396357fb5405e1227f
HuitingChiu <hana840101@gmail.com>
2022-11-25 22:52:31 +0800

    add main

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import *
+from flask_cors import CORS
 from mysql.connector import pooling
 import ssl
 import os
@@ -12,6 +13,8 @@ app = Flask(
 app.config["JSON_AS_ASCII"] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['JSON_SORT_KEYS'] = False
+
+CORS(app)
 
 # 使用.env隱藏私密訊息
 load_dotenv()
